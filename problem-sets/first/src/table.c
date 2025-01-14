6213f4a1dcd323e3c72ada46615242ec94ad6f76
phibkro <71797726+phibkro@users.noreply.github.com>
2025-01-14 16:42:18 +0100

    Reorganize into procedures

--- problem-sets/first/src/table.c
+++ problem-sets/first/src/table.c
@@ -12,46 +12,72 @@
 // The state to jump to as soon as a line is invalid
 #define ERROR 13
 
+// Main content, accept a repetition of statements
+#define BODY START + 2 // 3
+
+#define GO BODY + 2 // 4
+
+#define DXY GO + 2 // 6
+
 int table[NSTATES][256];
 
-void fillTable() {
+void fillTable()
+{
 
     // Make all states lead to ERROR by default
-    for (int i = 0; i < NSTATES; i++) {
-        for (int c = 0; c < 256; c++) {
+    for (int i = 0; i < NSTATES; i++)
+    {
+        for (int c = 0; c < 256; c++)
+        {
             table[i][c] = ERROR;
         }
     }
 
-    // Skip whitespace
-    table[START][' '] = START;
+    { /* START */
 
-    // If we reach a newline, and are not in the middle of a statement, accept
-    table[START]['\n'] = ACCEPT;
+        // Skip whitespace
+        table[START][' '] = START;
 
-    // Accept the statement "go"
-    table[START]['g'] = 1;
-    table[1]['o'] = 2;
-    table[2]['\n'] = ACCEPT;
+        // If we reach a newline, and are not in the middle of a statement, accept
+        table[START]['\n'] = ACCEPT;
 
-    // Accept repeated "go"s with spaces e.g "go go go" but not "gogogo"
-    table[2][' '] = START;
+        { /* START: STATEMENTS */
 
-    // Accept "d(x|y)=<number>" with leading zeros (007)
-    // Regex approximately d(x|y)=-?(0-9)+
-    table[START]['d'] = 3;
-    table[3]['x'] = 4;
-    table[3]['y'] = 4;
-    table[4]['='] = 5;
-    table[5]['-'] = 6;
-    for (char c = '0'; c <= '9'; c++)
-    {
-        table[5][c] = 6;
-        table[6][c] = 6;
+            table[START]['g'] = GO;
+            table[START]['d'] = DXY;
+        }
     }
-    table[6]['\n'] = ACCEPT;
-    // Loop
-    table[6][' '] = START;
-    
 
+    { /* BODY */
+
+        table[BODY]['g'] = GO;
+        table[BODY]['d'] = DXY;
+
+        { /* BODY: GO */
+
+            // Accept the statement "go"
+            table[GO]['o'] = GO + 1;
+            table[GO + 1]['\n'] = ACCEPT;
+
+            // Accept repeated "go"s with spaces e.g "go go go" but not "gogogo"
+            table[GO + 1][' '] = BODY;
+        }
+
+        { /* BODY: DXY */
+            // Accept "d(x|y)=<number>" with leading zeros (007)
+            // Regex approximately d(x|y)=-?(0-9)+
+            table[DXY]['x'] = DXY + 1;
+            table[DXY]['y'] = DXY + 1;
+            table[DXY + 1]['='] = DXY + 2;
+            table[DXY + 2]['-'] = DXY + 3;
+            for (char c = '0'; c <= '9'; c++)
+            {
+                table[DXY + 2][c] = DXY + 3;
+                table[DXY + 3][c] = DXY + 3;
+            }
+            table[DXY + 3]['\n'] = ACCEPT;
+            // Loop
+            table[DXY + 3][' '] = BODY;
+        }
+    }
 }
